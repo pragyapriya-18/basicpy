d0c785ed9ce50dff175df02180dadbfcd0279c4e
Pragya priya srivastava <pragyapriya.530@gmail.com>
2025-04-08 23:11:40 +0530

    Update 02_module_usage.py

--- 02_module_usage.py
+++ 02_module_usage.py
@@ -1,2 +1,5 @@
+# >>> from playsound import playsound
+# >>> playsound('/path/to/a/sound/file/you/want/to/play.mp3')
+
 from playsound import playsound
 playsound("C:\\Users\\Pragy\\Downloads\\midnight-ride-01a.mp3")
